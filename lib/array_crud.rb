97090a0c42cab6606ac2ff26eb3f48aa683532b9
John Loberger <johnloberger@gmail.com>
2020-04-27 18:28:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop("arrays!")
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift("wow")
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -27,13 +27,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[4] = ("totally")
 end
